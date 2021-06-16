4719dd22aea805104f29c9a1b0e456480fea44a5
Dimitri Glazkov <dimitri@glazkov.com>
2021-06-15 19:24:33 -0700

    Update the schema so far.

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,10 @@
+DROP TABLE `messages`;
 CREATE TABLE `messages` (
   `id` bigint(8) NOT NULL,
   `channel_id` bigint(8) NOT NULL,
   `guild_id` bigint(8) DEFAULT NULL,
   `author_id` bigint(8) DEFAULT NULL,
   `content` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
