32c0a3586f73a646e84db01a50fa89a88435ffa5
Dimitri Glazkov <dimitri@glazkov.com>
2021-06-18 21:32:14 -0700

    Implemented a very naive readback.

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,14 @@
-DROP TABLE `messages`;
+DROP TABLE IF EXISTS `channels`;
+
+CREATE TABLE `channels` (
+  `id` bigint(8) NOT NULL,
+  `is_fully_read` boolean,
+  `earliest_read_message` bigint(8) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `messages`;
+
 CREATE TABLE `messages` (
   `id` bigint(8) NOT NULL,
   `channel_id` bigint(8) NOT NULL,
@@ -6,6 +16,6 @@ CREATE TABLE `messages` (
   `author_id` bigint(8) DEFAULT NULL,
   `content` text NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  -- TODO: consider making channel_id a foreign key
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-SET NAMES utf8mb4;
--- scribe/scribe.go
+++ scribe/scribe.go
@@ -25,43 +25,85 @@ func asTime(s discordgo.Timestamp) time.Time {
 }
 
 func (s *Scribe) GetMessages(channelID string, guildID string) {
-	messages, err := s.reader.ChannelMessages(channelID, 100, "", "", "")
-	if err != nil {
-		log.Fatal(err)
-	}
 
+	// Wrap all this work in one transaction.
 	tx, err := s.db.BeginTx(s.ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer tx.Rollback()
 
-	tx.Exec("SET NAMES utf8mb4;") // make emoji be storable.
-
-	stmt, err := tx.Prepare(`INSERT INTO
-	messages (id, channel_id, guild_id, author_id, content, timestamp)
-	values (?, ?, ?, ?, ?, ?)`)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, message := range messages {
-		authorID := message.Author.ID
-		_, exec_err := stmt.Exec(
-			message.ID,
-			message.ChannelID,
-			guildID,
-			authorID,
-			message.Content,
-			asTime(message.Timestamp))
-
-		if exec_err != nil {
-			log.Fatalf("Error adding message: %v", exec_err)
+	// Check to see if we know about this channel.
+	var (
+		isFullyRead bool
+		beforeID    string
+		sqlBeforeID sql.NullString
+	)
+	row := tx.QueryRow(`
+		SELECT is_fully_read, earliest_read_message 
+		FROM channels WHERE id = ?`, channelID)
+	if err := row.Scan(&isFullyRead, &sqlBeforeID); err != nil {
+		if err == sql.ErrNoRows {
+			// we don't know about this channel, let's learn about it!
+			isFullyRead = false
+			beforeID = ""
+			if _, exec_err := tx.Exec(`
+				INSERT INTO channels (id, is_fully_read)
+				VALUES (?, ?)`, channelID, false); exec_err != nil {
+				log.Fatal(exec_err)
+			}
+		} else {
+			log.Fatal(err)
 		}
+	} else {
+		beforeID = sqlBeforeID.String
 	}
 
-	if err := tx.Commit(); err != nil {
-		log.Fatal(err)
+	if !isFullyRead {
+
+		messages, err := s.reader.ChannelMessages(channelID, 100, beforeID, "", "")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tx.Exec("SET NAMES utf8mb4;") // make emoji be storable.
+
+		stmt, err := tx.Prepare(`
+		INSERT INTO messages (id, channel_id, guild_id, author_id, content, timestamp)
+		VALUES (?, ?, ?, ?, ?, ?)`)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, message := range messages {
+			authorID := message.Author.ID
+			_, exec_err := stmt.Exec(
+				message.ID,
+				message.ChannelID,
+				guildID,
+				authorID,
+				message.Content,
+				asTime(message.Timestamp))
+
+			if exec_err != nil {
+				log.Fatalf("Error adding message: %v", exec_err)
+			}
+		}
+
+		beforeID = messages[len(messages)-1].ID
+		// TODO: consider the case when exactly 100 messages are read and 100th is the last one.
+		isFullyRead = len(messages) < 100
+
+		if _, err := tx.Exec(`
+		UPDATE channels SET is_fully_read = ?, earliest_read_message = ?`, isFullyRead, beforeID); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := tx.Commit(); err != nil {
+			log.Fatal(err)
+		}
+
 	}
 
 }
